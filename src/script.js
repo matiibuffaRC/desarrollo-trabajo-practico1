8d966ad94927bfbe87a1bf8c25a3cd784536d91d
matiibuffaRC <matbuffautn05@gmail.com>
2026-03-27 15:07:10 -0300

    ...

--- src/script.js
+++ src/script.js
@@ -1,31 +1,45 @@
-const toggle = document.getElementById('theme-toggle');
+const toggle = document.getElementById('theme-toggle'); // Boton de cambio de tema
 const html = document.documentElement;
 
 const menu = document.getElementById('menu');
-const menuBtn = document.getElementById('menu-btn');
+const menuBtn = document.getElementById('menu-btn'); // Menú de barras
 const overlay = document.getElementById('overlay');
 const header = document.getElementById('header');
 const spans = menuBtn.querySelectorAll('span');
-const links = document.querySelectorAll(".link");
-const sections = document.querySelectorAll("section"); 
+const links = document.querySelectorAll(".link"); 
+const sections = document.querySelectorAll("section"); // Secciones
+const btnTop = document.getElementById("btnTop"); // Boton de retorno al top
+const scrollIndicator = document.getElementById('scroll-indicator'); // Boton para bajar
+let isOpen = false; // Estado del menú mobile
 
 
-// Estado inicial hamburguesa
+// Estado inicial del menú mobile
 spans[0].style.transform = 'translateY(-6px)';
 spans[2].style.transform = 'translateY(6px)';
 
-// =======================
-// 🌙 DARK MODE
-// =======================
+
+// Cambiamos el tema y lo guardamos
 toggle.addEventListener('click', () => {
     html.classList.toggle('dark');
-    toggle.textContent = html.classList.contains('dark') ? '☀️' : '🌙';
+
+    const isDark = html.classList.contains('dark');
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+
+    toggle.style.transform = 'rotate(360deg)';
+
+    setTimeout(() => {
+        toggle.textContent = isDark ? '☀️' : '🌙';
+    }, 250);
+
+    setTimeout(() => {
+        toggle.style.transform = 'rotate(0deg)';
+    }, 500);
 });
 
-// =======================
-// 🍔 MENU
-// =======================
-let isOpen = false;
+if (localStorage.getItem('theme') === 'dark') {
+    html.classList.add('dark');
+    toggle.textContent = '☀️';
+}
 
 function openMenu() {
     menu.classList.remove('-translate-x-full');
@@ -48,8 +62,6 @@ function closeMenu() {
 
     overlay.classList.add('opacity-0', 'pointer-events-none');
     overlay.classList.remove('opacity-100');
-
-    // 🔥 volver blur
     header.classList.add('backdrop-blur');
 
     document.body.classList.remove('overflow-hidden');
@@ -110,9 +122,8 @@ window.addEventListener("scroll", () => {
 
 // Botón del scroll
 
-const btnTop = document.getElementById("btnTop");
-
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
+    // 🔝 Botón top
     if (window.scrollY > 200) {
         btnTop.classList.remove("opacity-0", "pointer-events-none");
         btnTop.classList.add("opacity-100");
@@ -120,9 +131,19 @@ window.addEventListener("scroll", () => {
         btnTop.classList.add("opacity-0", "pointer-events-none");
         btnTop.classList.remove("opacity-100");
     }
+    const maxScroll = 150;
+    const opacity = 1 - window.scrollY / maxScroll;
+
+    scrollIndicator.style.opacity = opacity;
+
+    if (window.scrollY > maxScroll) {
+        scrollIndicator.style.pointerEvents = 'none';
+    } else {
+        scrollIndicator.style.pointerEvents = 'auto';
+    }
 });
 
-// Scroll hacia arriba
+
 btnTop.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
@@ -132,7 +153,7 @@ btnTop.addEventListener("click", () => {
 
 // 
 
-const scrollIndicator = document.getElementById('scroll-indicator');
+// Boton para scroll para abajo
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
